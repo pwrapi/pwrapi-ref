041909a30a85789e5d2b1a624f249733110a5093
David DeBonis <ddeboni@sandia.gov>
2014-04-30 19:39:58 +0000

    migrating pidev into mchw specific device
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@172 bcb1eb47-8062-464b-8553-523ad38f42de

--- mchw_pidev.c
+++ mchw_pidev.c
@@ -1,27 +1,27 @@
 #include "mchw_pidev.h"
 #include "pidev.h"
 
-void mchw_pidev_init_t(mchw_dev_t *dev)
+void mchw_pidev_init(mchw_dev_t *dev)
 {
 }
 
-void mchw_pidev_final_t(mchw_dev_t *dev)
+void mchw_pidev_final(mchw_dev_t *dev)
 {
 }
 
-void mchw_pidev_open_t(mchw_dev_t *dev)
+void mchw_pidev_open(mchw_dev_t *dev)
 {
 }
 
-void mchw_pidev_close_t(mchw_dev_t *dev)
+void mchw_pidev_close(mchw_dev_t *dev)
 {
 }
 
-void mchw_pidev_read_t(mchw_dev_t *dev, mchw_reading_t *reading)
+void mchw_pidev_read(mchw_dev_t *dev, mchw_reading_t *reading)
 {
 }
 
-void mchw_pidev_time_t(mchw_dev_t *dev, mchw_time_t *time)
+void mchw_pidev_time(mchw_dev_t *dev, mchw_time_t *time)
 {
 }
 
