49617cf8d224e85544f69d47d8fbe7fedc9afc8a
Michael Levenhagen <mjleven@sandia.gov>
2014-06-02 22:02:43 +0000

    fix bad name
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@241 bcb1eb47-8062-464b-8553-523ad38f42de

--- dev.h
+++ dev.h
@@ -11,8 +11,8 @@ typedef void* pwr_dev_t;
 
 typedef pwr_dev_t (*pwr_open_t)( const char *initstr );
 typedef int (*pwr_close_t)( pwr_dev_t dev );
-typedef int (*pwr_readv_t)( pwr_dev_t dev, unsigned int arraysize, PWR_Value values[], int v[] );
-typedef int (*pwr_writev_t)( pwr_dev_t dev, unsigned int arraysize, PWR_Value values[], int v[] );
+typedef int (*pwr_readv_t)( pwr_dev_t dev, unsigned int arraysize, PWR_Value values[], int status[] );
+typedef int (*pwr_writev_t)( pwr_dev_t dev, unsigned int arraysize, PWR_Value values[], int status[] );
 typedef int (*pwr_time_t)( pwr_dev_t dev, unsigned long long *time );
 typedef int (*pwr_read_t)( pwr_dev_t dev, PWR_AttrType, void*, size_t, PWR_Time*  );
 typedef int (*pwr_write_t)( pwr_dev_t dev, PWR_AttrType, void*, size_t );
