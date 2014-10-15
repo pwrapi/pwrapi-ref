49322e3ed5eaeef1bd0d8896d131aceb315083c1
David DeBonis <ddeboni@sandia.gov>
2014-10-15 22:11:17 +0000

    testing power gadget
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@598 bcb1eb47-8062-464b-8553-523ad38f42de

--- examples/main.c
+++ examples/main.c
@@ -1,5 +1,5 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <assert.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -7,40 +7,88 @@
 
 int main( int argc, char* argv[] )
 {
-    PWR_Obj   self;
+    PWR_Obj self;
     PWR_Cntxt cntxt;
-    time_t    time;
-    double    energy_start = 0.0, energy_end = 0.0;
-    PWR_Time  energy_start_ts = 0, energy_end_ts = 0;
+    time_t time;
+    double start = 0.0, val = 0.0;
+    PWR_Time start_ts = 0, val_ts = 0;
+    PWR_AttrName attr = PWR_ATTR_ENERGY;
+
+    unsigned int i, option, verbose = 0, samples = 1, freq = 1;
+    static char usage[] =
+        "usage: %s [-s samples] [-f freq] [-a attr] [-v] [-h]\n";
+
+    while( (option=getopt( argc, argv, "s:f:a:vh" )) != -1 )
+        switch( option ) {
+            case 's':
+                samples = atoi(optarg);
+                break;
+            case 'f':
+                freq = atoi(optarg);
+                break;
+            case 'a':
+                switch( optarg[0] ) {
+                    case 'E':
+                        attr = PWR_ATTR_ENERGY;
+                        break;
+                    case 'P':
+                        attr = PWR_ATTR_POWER;
+                        break;
+                    case 'F':
+                        attr = PWR_ATTR_FREQ;
+                        break;
+                    case 'T':
+                        attr = PWR_ATTR_TEMP;
+                        break;
+                    default:
+                        printf( "Error: unsupported attribute type (try E P F or T)\n" );
+                        return -1;
+                } 
+                break;
+            case 'v':
+                verbose = 1;
+                break;
+            case 'h':
+            case '?':
+                fprintf( stderr, usage, argv[0] );
+                exit( 1 );
+        }
+
+    if( verbose )
+        printf( "samples=%d, freq=%d, attr=%d\n", samples, freq, attr );
 
     if( (cntxt=PWR_CntxtInit( PWR_CNTXT_DEFAULT, PWR_ROLE_APP, "MiniMD" )) == 0x0 ) {
-      printf( "Error: initialization of PowerAPI context failed\n" );
-      return -1;
+        printf( "Error: initialization of PowerAPI context failed\n" );
+        return -1;
     }
 
     if( (self=PWR_CntxtGetEntryPoint( cntxt )) == 0x0 ) {
-      printf( "Error: getting self from PowerAPI context failed\n" );
-      return -1;
+        printf( "Error: getting self from PowerAPI context failed\n" );
+        return -1;
     }
-    printf("Profiling `%s`\n", PWR_ObjGetTypeString( PWR_ObjGetType( self ) ) ); 
+    if( verbose )
+        printf( "Profiling `%s`\n", PWR_ObjGetTypeString( PWR_ObjGetType( self ) ) ); 
 
-    if( PWR_ObjAttrGetValue( self, PWR_ATTR_ENERGY, &energy_start, &energy_start_ts ) == PWR_RET_INVALID ) {
-      printf( "Error: getting attribute value from PowerAPI self failed\n" );
-      return -1;
+    for( i = 0; i < samples; i++ ) {
+        if( PWR_ObjAttrGetValue( self, attr, &val, &val_ts ) == PWR_RET_INVALID ) {
+            printf( "Error: reading of PowerAPI attribute failed\n" );
+            return -1;
+        }
+
+        if( !i && attr == PWR_ATTR_ENERGY ) {
+            start = val;
+            start_ts = val_ts;
+        }
+
+        PWR_TimeConvert( val_ts, &time );
+        printf( "Value is %lf at time %s", val, ctime(&time) );
+
+        usleep( 1000000.0 / freq);
     }
-    PWR_TimeConvert( energy_start_ts, &time );
-    printf( "Starting energy is %lf Joules on %s", energy_start, ctime(&time) );
 
-    sleep( 10 );
-
-    if( PWR_ObjAttrGetValue( self, PWR_ATTR_ENERGY, &energy_end, &energy_end_ts ) == PWR_RET_INVALID ) {
-      return -1;
-    }
-    PWR_TimeConvert( energy_end_ts, &time );
-    printf( "Ending energy is %lf Joules on %s", energy_end, ctime(&time) );
-
-    printf( "Total energy is %lf Joules over %f seconds\n", energy_end - energy_start,
-            (unsigned long long)(energy_end_ts - energy_start_ts)/1000000000.0 );
+    if( attr == PWR_ATTR_ENERGY )
+        printf( "Total energy is %lf Joules over %f seconds\n", val - start,
+                (unsigned long long)(val_ts - start_ts)/1000000000.0 );
 
     return 0;
 }
