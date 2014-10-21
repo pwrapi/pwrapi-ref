726fb683cd6319b8099b44dd18cd71145dedde96
David DeBonis <ddeboni@sandia.gov>
2014-10-21 19:00:36 +0000

    reflect XML update
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@639 bcb1eb47-8062-464b-8553-523ad38f42de

--- examples/run.sh
+++ examples/run.sh
@@ -3,7 +3,7 @@ ROOT=${PWD}/..
 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${ROOT}/pow:${ROOT}/plugins"
 export POWERAPI_CONFIG="${ROOT}/examples/pg-node.xml"
-export POWERAPI_ROOT="plat.node0"
+export POWERAPI_ROOT="plat.node"
 
 SAMPLES=100
 FREQ=10
