5c213238297e37e1a7624236d08af9ca6a73117f
Michael Levenhagen <mjleven@sandia.gov>
2014-10-23 14:44:54 +0000

    add copyright
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@641 bcb1eb47-8062-464b-8553-523ad38f42de

--- pow/util.h
+++ pow/util.h
@@ -1,3 +1,9 @@
+/* 
+ * Copyright 2014 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this work 
+ * by or on behalf of the U.S. Government. Export of this program may require
+ * a license from the United States Government.
+ */
 
 #ifndef _PWR_UTIL_H
 #define _PWR_UTIL_H
