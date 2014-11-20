84c6b8dca283184a051cd13f07ae06bb77f47856
Michael Levenhagen <mjleven@sandia.gov>
2014-11-20 16:13:12 +0000

    update copyright and license
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@716 bcb1eb47-8062-464b-8553-523ad38f42de

--- examples/powerapi.c
+++ examples/powerapi.c
@@ -1,9 +1,14 @@
-/* 
+/*
  * Copyright 2014 Sandia Corporation. Under the terms of Contract
- * DE-AC04-94AL85000, there is a non-exclusive license for use of this work 
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this work
  * by or on behalf of the U.S. Government. Export of this program may require
  * a license from the United States Government.
- */
+ *
+ * This file is part of the Power API Prototype software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+*/
+
 
 #include <stdlib.h>
 #include <stdio.h>
--- examples/run_dummy.sh
+++ examples/run_dummy.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+#
+# Copyright 2014 Sandia Corporation. Under the terms of Contract
+# DE-AC04-94AL85000, there is a non-exclusive license for use of this work
+# by or on behalf of the U.S. Government. Export of this program may require
+# a license from the United States Government.
+#
+# This file is part of the Power API Prototype software package. For license
+# information, see the LICENSE file in the top level directory of the
+# distribution.
+#
+
+
 ROOT=${PWD}/..
 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${ROOT}/pow:${ROOT}/plugins"
