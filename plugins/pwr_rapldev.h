e52ab14172c3960024a87cd79977eefa09982d6e
Michael Levenhagen <mjleven@sandia.gov>
2014-11-20 16:19:08 +0000

    update copyright and license
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@722 bcb1eb47-8062-464b-8553-523ad38f42de

--- plugins/pwr_rapldev.h
+++ plugins/pwr_rapldev.h
@@ -1,9 +1,13 @@
-/* 
+/*
  * Copyright 2014 Sandia Corporation. Under the terms of Contract
- * DE-AC04-94AL85000, there is a non-exclusive license for use of this work 
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this work
  * by or on behalf of the U.S. Government. Export of this program may require
  * a license from the United States Government.
- */
+ *
+ * This file is part of the Power API Prototype software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+*/
 
 #ifndef PWR_RAPLDEV_H 
 #define PWR_RAPLDEV_H 
