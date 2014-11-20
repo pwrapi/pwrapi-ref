23acd8f88ad2b902bf482e56adacc0837bbf34a6
Michael Levenhagen <mjleven@sandia.gov>
2014-11-20 16:14:23 +0000

    update coypright and license
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@719 bcb1eb47-8062-464b-8553-523ad38f42de

--- pow/stat.h
+++ pow/stat.h
@@ -3,7 +3,11 @@
  * DE-AC04-94AL85000, there is a non-exclusive license for use of this work 
  * by or on behalf of the U.S. Government. Export of this program may require
  * a license from the United States Government.
- */
+ *
+ * This file is part of the Power API Prototype software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+*/
 
 #ifndef _PWR_STAT_H
 #define _PWR_STAT_H
