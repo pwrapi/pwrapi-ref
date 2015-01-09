6a19a905eb4fb3f50fbb5eeebbf126c426a96665
David DeBonis <ddeboni@sandia.gov>
2015-01-09 16:25:49 +0000

    added copyright banner to source file
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@765 bcb1eb47-8062-464b-8553-523ad38f42de

--- hwloc/hwloc.cpp
+++ hwloc/hwloc.cpp
@@ -1,3 +1,14 @@
+/* 
+ * Copyright 2014 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this work 
+ * by or on behalf of the U.S. Government. Export of this program may require
+ * a license from the United States Government.
+ *
+ * This file is part of the Power API Prototype software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+*/
+
 /* Simple utility for generating a topology for use with the powerapi prototype 
  * Creates a file hwloc.xml formatted appropriately.
  */
