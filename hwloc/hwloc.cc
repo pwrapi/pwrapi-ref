683bba564ff5a024a9062105e20f764c9f1a247c
David DeBonis <ddeboni@sandia.gov>
2015-01-09 17:13:32 +0000

    added filename for input and general style cleanup
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@767 bcb1eb47-8062-464b-8553-523ad38f42de

--- hwloc/hwloc.cc
+++ hwloc/hwloc.cc
@@ -19,105 +19,130 @@
 using namespace tinyxml2;
 
 // Translates the hwloc XML to powerapi terminology.
-int translate(XMLDocument* doc, XMLElement* e, XMLElement* parent, int index){
-  int initial = index;
-  XMLElement * hwloc_object = e->FirstChildElement("object");
-  while(hwloc_object != NULL){
-    char name[256];
-    if(hwloc_object->Attribute("type", "Machine")){
-      XMLElement * child = doc->NewElement("obj");
-      snprintf(name, 256, "node%d", index++);
-      child->SetAttribute("name", name); 
-      child->SetAttribute("type", "Node"); 
-      parent->InsertEndChild(child);
-      translate(doc, hwloc_object, child, 0);
-    } else if (hwloc_object->Attribute("type", "Socket")){
-      XMLElement * child = doc->NewElement("obj");
-      snprintf(name, 256, "socket%d", index++);
-      child->SetAttribute("name", name); 
-      child->SetAttribute("type", "Socket"); 
-      parent->InsertEndChild(child);
-      translate(doc, hwloc_object, child, 0);
-    } else if (hwloc_object->Attribute("type", "Core")){
-      XMLElement * child = doc->NewElement("obj");
-      snprintf(name, 256, "core%d", index++);
-      child->SetAttribute("name", name); 
-      child->SetAttribute("type", "Core"); 
-      parent->InsertEndChild(child);
-      translate(doc, hwloc_object, child, 0);
-    } else {
-      index += translate(doc, hwloc_object, parent, index);
+int translate( XMLDocument* doc, XMLElement* e, XMLElement* parent, int index )
+{
+    int initial = index;
+    XMLElement * hwloc_object = e->FirstChildElement( "object" );
+
+    while( hwloc_object != NULL ) {
+        char name[256];
+
+        if( hwloc_object->Attribute( "type", "Machine" ) ) {
+            XMLElement * child = doc->NewElement( "obj" );
+            snprintf( name, 256, "node%d", index++ );
+            child->SetAttribute( "name", name ); 
+            child->SetAttribute( "type", "Node" ); 
+            parent->InsertEndChild( child );
+            translate( doc, hwloc_object, child, 0 );
+        } else if( hwloc_object->Attribute( "type", "Socket" ) ) {
+            XMLElement * child = doc->NewElement( "obj" );
+            snprintf( name, 256, "socket%d", index++ );
+            child->SetAttribute( "name", name ); 
+            child->SetAttribute( "type", "Socket" ); 
+            parent->InsertEndChild( child );
+            translate( doc, hwloc_object, child, 0 );
+        } else if( hwloc_object->Attribute( "type", "Core" ) ) {
+            XMLElement * child = doc->NewElement( "obj" );
+            snprintf( name, 256, "core%d", index++ );
+            child->SetAttribute( "name", name ); 
+            child->SetAttribute( "type", "Core" ); 
+            parent->InsertEndChild( child );
+            translate( doc, hwloc_object, child, 0 );
+        } else {
+            index += translate( doc, hwloc_object, parent, index );
+        }
+
+        hwloc_object = hwloc_object->NextSiblingElement( "object" );
     }
-    hwloc_object = hwloc_object->NextSiblingElement("object");
-  }
-  return index - initial;
+
+    return index - initial;
 }
 
 // Flattens the XML objects so that children are not XML children :(
-void flatten(XMLDocument* doc, XMLElement* e, XMLElement* objs, char* prefix){
-  XMLElement * children = doc->NewElement("children");
-  XMLElement * child = e->FirstChildElement("obj");
-  char name[256];
-  while(child != NULL){
-    XMLElement * c = doc->NewElement("child");
-    c->SetAttribute("name", child->Attribute("name"));
-    children->InsertEndChild(c);
-    snprintf(name, 256, "%s.%s", prefix, child->Attribute("name"));
-    child->SetAttribute("name", name);
-    XMLElement * newchild = child->NextSiblingElement("obj");
-    objs->InsertEndChild(child);
-    flatten(doc, child, objs, name);
-    child = newchild;
-  }
-  e->InsertEndChild(children);
+void flatten(XMLDocument* doc, XMLElement* e, XMLElement* objs, char* prefix)
+{
+    XMLElement * children = doc->NewElement( "children" );
+    XMLElement * child = e->FirstChildElement( "obj" );
+    char name[256];
+
+    while( child != NULL ) {
+        XMLElement * c = doc->NewElement( "child" );
+        c->SetAttribute( "name", child->Attribute( "name" ) );
+        children->InsertEndChild( c );
+        snprintf( name, 256, "%s.%s", prefix, child->Attribute( "name" ) );
+        child->SetAttribute( "name", name );
+        XMLElement * newchild = child->NextSiblingElement( "obj" );
+        objs->InsertEndChild( child );
+        flatten( doc, child, objs, name );
+        child = newchild;
+    }
+
+    e->InsertEndChild( children );
 }
 
-int main(){
-  unsigned int err;
+int main( int argc, char** argv )
+{
+    const char USAGE[] = "usage: %s [-f filename]\n";
+    const char FILENAME[] = "hwloc.xml";
 
-  // Load hwloc document, create new doc
-  err = system("lstopo hwloc.xml -f");
-  if(err){
-    printf("lstopo failed, is hwloc installed?\n");
-    return err;
-  }
+    char syscall[256];
+    char *filename = (char *)FILENAME;
+    int option;
 
-  XMLDocument doc;
-  doc.LoadFile("hwloc.xml");
-  XMLDocument newdoc;
+    while( (option=getopt( argc, argv, "f:h" )) != -1 )
+        switch( option ) {
+            case 'f':
+                filename = optarg;
+                break;
+            default:
+                fprintf( stderr, USAGE, argv[0] );
+                return -1;
+        }
 
-  // System
-  XMLNode * system = newdoc.NewElement("System"); 
-  newdoc.InsertEndChild(system);
+    // Load hwloc document, create new doc
+    unsigned int err;
+    snprintf( syscall, 256, "lstopo %s", filename );
+    if( (err=system( syscall )) ) {
+        printf( "lstopo failed, is hwloc installed?\n" );
+        return err;
+    }
+
+    XMLDocument doc, newdoc;
+    doc.LoadFile( filename );
+
+    // System
+    XMLNode * system = newdoc.NewElement( "System" ); 
+    newdoc.InsertEndChild( system );
   
-  // Plugins
-  XMLElement * plugins = newdoc.NewElement("Plugins");
-  system->InsertEndChild(plugins); 
+    // Plugins
+    XMLElement * plugins = newdoc.NewElement( "Plugins" );
+    system->InsertEndChild( plugins ); 
 
-  // Devices
-  XMLElement * devices = newdoc.NewElement("Devices");
-  system->InsertEndChild(devices);
+    // Devices
+    XMLElement * devices = newdoc.NewElement( "Devices" );
+    system->InsertEndChild( devices );
 
-  // Objects
-  XMLElement * objects = newdoc.NewElement("Objects");
-  system->InsertEndChild(objects);
+    // Objects
+    XMLElement * objects = newdoc.NewElement( "Objects" );
+    system->InsertEndChild( objects );
   
-  // Initialize with platform
-  XMLElement * obj = newdoc.NewElement("obj");
-  obj->SetAttribute("name", "plat");
-  obj->SetAttribute("type", "Platform");
-  objects->InsertEndChild(obj);
+    // Initialize with platform
+    XMLElement * obj = newdoc.NewElement( "obj" );
+    obj->SetAttribute( "name", "plat" );
+    obj->SetAttribute( "type", "Platform" );
+    objects->InsertEndChild( obj );
   
-  // Iterate through hwloc xml, translating to powerapi topology
-  translate(&newdoc, doc.FirstChildElement(), obj, 0);
-  flatten(&newdoc, obj, objects, (char*)"plat");
+    // Iterate through hwloc xml, translating to powerapi topology
+    translate( &newdoc, doc.FirstChildElement(), obj, 0 );
+    flatten( &newdoc, obj, objects, (char*)"plat" );
 
-  // Save output
-  err = newdoc.SaveFile("hwloc.xml");
-  if(err){
-    printf("Error writing to hwloc.xml\n");
-    return err;
-  } else {
-    printf("Output written to hwloc.xml\n");
-  }
+    // Save output
+    if( (err=newdoc.SaveFile( filename )) ) {
+        printf( "Error writing to %s\n", filename );
+        return err;
+    } else {
+        printf( "Output written to %s\n", filename );
+    }
+
+    return 0;
 }
