4327d204d4a6d89746ac7578b43159736f6f823b
David DeBonis <ddeboni@sandia.gov>
2015-05-01 17:40:47 +0000

    added powergrp utility
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@907 bcb1eb47-8062-464b-8553-523ad38f42de

---
+++ tools/powergrp.c
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2014 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this work
+ * by or on behalf of the U.S. Government. Export of this program may require
+ * a license from the United States Government.
+ *
+ * This file is part of the Power API Prototype software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "pow.h"
+
+#define NUM_ATTR(X) (sizeof(X)/sizeof(PWR_AttrName))
+
+int main( int argc, char* argv[] )
+{
+    PWR_Obj self, obj;
+    PWR_Grp grp;
+    PWR_Cntxt cntxt;
+    size_t size;
+    unsigned long long time;
+    double start = 0.0;
+    PWR_Time start_ts = 0;
+
+    PWR_AttrName attrs[] = { PWR_ATTR_CURRENT, PWR_ATTR_POWER, PWR_ATTR_ENERGY };
+    PWR_Time vals_ts[NUM_ATTR(attrs)];
+    double vals[NUM_ATTR(attrs)];
+    int stats[NUM_ATTR(attrs)];
+
+    unsigned int i, j, option, samples = 1, freq = 1;
+    static char usage[] =
+        "usage: %s [-s samples] [-f freq] [-h]\n";
+
+    while( (option=getopt( argc, argv, "s:f:h" )) != -1 )
+        switch( option ) {
+            case 's':
+                samples = atoi(optarg);
+                break;
+            case 'f':
+                freq = atoi(optarg);
+                break;
+            case 'h':
+            case '?':
+                fprintf( stderr, usage, argv[0] );
+                exit( 1 );
+        }
+
+    if( (cntxt=PWR_CntxtInit( PWR_CNTXT_DEFAULT, PWR_ROLE_APP, "Application" )) == 0x0 ) {
+        printf( "Error: initialization of PowerAPI context failed\n" );
+        return -1;
+    }
+
+    if( (self=PWR_CntxtGetEntryPoint( cntxt )) == 0x0 ) {
+        printf( "Error: getting self from PowerAPI context failed\n" );
+        return -1;
+    }
+/*
+    if( (obj=PWR_CntxtGetObjByName( cntxt, "teller.node40" )) == 0x0 ) {
+        printf( "Error: getting object by name from PowerAPI context failed\n" );
+        return -1;
+    }
+*/
+    printf("GETTING CHILDREN\n");
+    if( (grp=PWR_ObjGetChildren( self )) == 0x0 ) {
+        printf( "Error: getting object by name from PowerAPI context failed\n" );
+        return -1;
+    }
+    size = PWR_GrpGetNumObjs( grp );
+    for( i = 0; i < size; i++ ) {
+        obj = PWR_GrpGetObjByIndx( grp, i );
+        if( !(strcmp( PWR_ObjGetName( obj ), "teller.node40" )) ||
+            !(strcmp( PWR_ObjGetName( obj ), "teller.node41" )) ||
+            !(strcmp( PWR_ObjGetName( obj ), "teller.node42" )) ||
+            !(strcmp( PWR_ObjGetName( obj ), "teller.node43" )) )
+            PWR_GrpAddObj( grp, obj );
+    }
+
+    for( i = 0; i < samples; i++ ) {
+        if( PWR_ObjAttrGetValues( obj, NUM_ATTR(attrs), attrs, vals, vals_ts, stats ) == PWR_RET_INVALID ) {
+            printf( "Error: reading of PowerAPI attributes failed\n" );
+            return -1;
+        }
+        for( j = 0; j < NUM_ATTR(attrs); j++ ) {
+            if( !i && attrs[j] == PWR_ATTR_ENERGY ) {
+                start = vals[j];
+                start_ts = vals_ts[j];
+            }
+            if( !j ) time = vals_ts[j] - start_ts;
+            if( attrs[j] == PWR_ATTR_ENERGY )
+                printf( "%lf ", vals[j] - start );
+            printf( "%lf ", vals[j] );
+        }
+        printf( "%lf", time/1000000000.0 );
+
+        usleep( 1000000.0 / freq);
+    }
+
+    return 0;
+}
---
+++ tools/run-powergrp.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#
+# Copyright 2014 Sandia Corporation. Under the terms of Contract
+# DE-AC04-94AL85000, there is a non-exclusive license for use of this work
+# by or on behalf of the U.S. Government. Export of this program may require
+# a license from the United States Government.
+#
+# This file is part of the Power API Prototype software package. For license
+# information, see the LICENSE file in the top level directory of the
+# distribution.
+#
+
+
+ROOT=${PWD}/..
+
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${ROOT}/pow:${ROOT}/plugins"
+export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:${ROOT}/pow:${ROOT}/plugins"
+export POWERAPI_CONFIG="${ROOT}/tools/config/teller-platform.xml"
+export POWERAPI_ROOT="teller"
+
+SAMPLES=1
+FREQ=1
+
+./powergrp -s ${SAMPLES} -f ${FREQ}
+
