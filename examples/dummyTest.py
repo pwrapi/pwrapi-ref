b5539e62cd75bfd3c5f2d0f90230e01b2e87698f
Michael Levenhagen <mjleven@sandia.gov>
2015-05-27 21:55:51 +0000

    fix up
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/PowerAPI/src/prototype@989 bcb1eb47-8062-464b-8553-523ad38f42de

--- examples/dummyTest.py
+++ examples/dummyTest.py
@@ -11,7 +11,7 @@
 # distribution.
 #
 
-import _pwr as pow 
+import pwr as pow 
 import time
 
 cntxt = pow.CntxtInit( pow.CNTXT_DEFAULT, pow.ROLE_APP, "App" )
@@ -22,7 +22,7 @@ assert( self )
 
 objName = pow.ObjGetTypeString( pow.ObjGetType( self ) )
 
-print "I'm a {}".format( objName )
+print "I'm a {0}".format( objName )
 
 parent = pow.ObjGetParent( self )
 assert( 0 != parent  )
@@ -31,7 +31,7 @@ children = pow.ObjGetChildren( self );
 assert( children )
 
 for i in range(0,pow.GrpGetNumObjs(children)): 
-	print "child {}".format( pow.ObjGetName( pow.GrpGetObjByIndx( children, i ) ) ) 
+	print "child {0}".format( pow.ObjGetName( pow.GrpGetObjByIndx( children, i ) ) ) 
 
 retval,value,ts = pow.ObjAttrGetValue( self, pow.ATTR_VOLTAGE )
 assert( retval == pow.RET_INVALID )
@@ -41,10 +41,10 @@ assert( retval == pow.RET_SUCCESS )
 
 retval, timevalue = pow.TimeConvert( ts )
 
-print "ObjAttrGetValue(ATTR_POWER) value={} ts={}".format(value,time.ctime(timevalue))
+print "ObjAttrGetValue(ATTR_POWER) value={0} ts={1}".format(value,time.ctime(timevalue))
 
 value = 25.812;
-print "ObjAttrSetValue(ATTR_POWER) value={}".format(value)
+print "ObjAttrSetValue(ATTR_POWER) value={0}".format(value)
 retval = pow.ObjAttrSetValue( self, pow.ATTR_POWER, value );
 assert( retval == pow.RET_SUCCESS );
 
@@ -57,10 +57,10 @@ assert( retval == pow.RET_SUCCESS )
 
 retval, timevalue = pow.TimeConvert( ts );
 
-print "ObjAttrGetValues(ATTR_POWER) value={} ts={}".format(value,time.ctime(timevalue))
+print "ObjAttrGetValues(ATTR_POWER) value={0} ts={1}".format(value,time.ctime(timevalue))
 
 value = [100.10]
-print "ObjAttrSetValues(ATTR_POWER) value={}".format(value)
+print "ObjAttrSetValues(ATTR_POWER) value={0}".format(value)
 retval = pow.ObjAttrSetValues( self, name, value, status )
 assert( retval == pow.RET_SUCCESS );
 
@@ -70,14 +70,14 @@ assert( retval == pow.RET_SUCCESS )
 assert( value == 200.20 );
 
 retval, timevalue = pow.TimeConvert( ts );
-print "ObjAttrGetValue(ATTR_POWER) value={} ts={}".format(value,time.ctime(timevalue));
+print "ObjAttrGetValue(ATTR_POWER) value={0} ts={1}".format(value,time.ctime(timevalue));
 
 grp = pow.CntxtGetGrpByType( cntxt, pow.OBJ_CORE )
 assert( grp );
 assert( pow.GrpGetNumObjs( grp ) )
 
 value = 0.1
-print "GrpAttrSetValue(ATTR_POWER) value={}\n".format(value)
+print "GrpAttrSetValue(ATTR_POWER) value={0}\n".format(value)
 retval = pow.GrpAttrSetValue( grp, pow.ATTR_POWER, value, status )
 assert( retval == pow.RET_SUCCESS )
 
@@ -86,7 +86,7 @@ assert( retval == pow.RET_SUCCESS )
 assert( value == 0.2 );
 
 retval, timevalue = pow.TimeConvert( ts )
-print "ObjAttrGetValue(ATTR_POWER) value={} ts={}".format( value,time.ctime(timevalue))
+print "ObjAttrGetValue(ATTR_POWER) value={0} ts={1}".format( value,time.ctime(timevalue))
 
 core = pow.GrpGetObjByIndx( grp, 0 );
 assert( core )
@@ -102,13 +102,13 @@ time.sleep( 0.00001 )
 retval, value, statTimes = pow.StatGetValue( coreStat )
 assert( retval == pow.RET_SUCCESS )
 
-print "StatGetValue(ATTR_POWER) value={}".format( value )
-print "StatGetValue(ATTR_POWER) start={}".format( statTimes.start )
+print "StatGetValue(ATTR_POWER) value={0}".format( value )
+print "StatGetValue(ATTR_POWER) start={0}".format( statTimes.start )
 
-print "StatGetValue(ATTR_POWER) stop={}".format( statTimes.stop )
+print "StatGetValue(ATTR_POWER) stop={0}".format( statTimes.stop )
 
 if ( statTimes.instant != pow.TIME_NOT_SET ):
-    print "StatGetValue(ATTR_POWER) instant={}".format( statTimes.instant )
+    print "StatGetValue(ATTR_POWER) instant={0}".format( statTimes.instant )
 
 pow.StatDestroy( coreStat )
 
