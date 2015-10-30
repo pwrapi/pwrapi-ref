6bb9ef1f2a3fd692ee675551dc0260333d6aa60e
David DeBonis <ddeboni@sandia.gov>
2015-10-30 12:09:00 -0600

    fix for typos in pwrtypes and cleanup of pwrdmp

--- tools/pwrdmp.c
+++ tools/pwrdmp.c
@@ -17,124 +17,31 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-void dump_object( PWR_Obj self )
-{
-    PWR_ObjType type = PWR_GetObjType( self );
-    char typestr[20] = "";
-
-    switch( type ) {
-        case PWR_OBJ_PLATFORM:
-            strcpy( typestr, "Platform");
-            break;
-        case PWR_OBJ_BOARD:
-            strcpy( typestr, "Board");
-            break;
-        case PWR_OBJ_NODE:
-            strcpy( typestr, "Node");
-            break;
-        case PWR_OBJ_SOCKET:
-            strcpy( typestr, "Socket");
-            break;
-        case PWR_OBJ_CORE:
-            strcpy( typestr, "Core");
-            break;
-        case PWR_OBJ_MEM:
-            strcpy( typestr, "Memory");
-            break;
-        case PWR_OBJ_NIC:
-            strcpy( typestr, "NIC");
-            break;
-        default:
-            strcpy( typestr, "Invalid");
-            break;
-    }
-
-    switch( type ) {
-        case PWR_ATTR_NAME:
-            strcpy( attrstr, "Name");
-            break;
-        case PWR_ATTR_FREQ:
-            strcpy( attrstr, "Frequency");
-            break;
-        case PWR_ATTR_MAX_PCAP:
-            strcpy( attrstr, "Maximum PowerCap");
-            break;
-        case PWR_ATTR_MIN_PCAP:
-            strcpy( attrstr, "Minimum PowerCap");
-            break;
-        case PWR_ATTR_MAX_POWER:
-            strcpy( attrstr, "Maximum Power");
-            break;
-        case PWR_ATTR_MIN_POWER:
-            strcpy( attrstr, "Minimum Power");
-            break;
-        case PWR_ATTR_AVG_POWER:
-            strcpy( attrstr, "Average Power");
-            break;
-        case PWR_ATTR_POWER:
-            strcpy( attrstr, "Power");
-            break;
-        case PWR_ATTR_VOLTAGE:
-            strcpy( attrstr, "Voltage");
-            break;
-        case PWR_ATTR_CURRENT:
-            strcpy( attrstr, "Current");
-            break;
-        case PWR_ATTR_ENERGY:
-            strcpy( attrstr, "Energy");
-            break;
-        case PWR_ATTR_TEMP:
-            strcpy( attrstr, "Temperature");
-            break;
-        case PWR_ATTR_PSTATE:
-            strcpy( attrstr, "P-State");
-            break;
-        case PWR_ATTR_CSTATE:
-            strcpy( attrstr, "C-State");
-            break;
-        case PWR_ATTR_SSTATE:
-            strcpy( attrstr, "S-State");
-            break;
-        case PWR_ATTR_FREQ_LIMIT_MAX:
-            strcpy( attrstr, "Frequency Limit Maximum");
-            break;
-        case PWR_ATTR_FREQ_LIMIT_MIN:
-            strcpy( attrstr, "Frequency Limit Minimum");
-            break;
-        case PWR_ATTR_NOT_SPECIFIED:
-            strcpy( attrstr, "Not Specified");
-            break;
-        case PWR_ATTR_INVALID:
-        default:
-            strcpy( attrstr, "Invalid");
-            break;
-    }
-
-    printf( "%s %s %s\n", PWR_GetObjName( self ), typestr, attrstr ); 
-}
 
 void dump_type_objects( PWR_Obj self, PWR_ObjType type )
 {
-    unsigned int i, j, count = 0;
+    unsigned int i, j, k, count = 0;
     size_t size;
     PWR_Grp grp;
     PWR_Obj obj;
 
-    if( (grp=PWR_ObjGetChildren( self )) == (PWR_Grp)0x0 ) {
-        printf( "Error: getting child objects from PowerAPI context failed\n" );
-        return (PWR_Grp)0x0;
-    }
+    if( (grp=PWR_ObjGetChildren( self )) == (PWR_Grp)0x0 )
+        return;
+
     size = PWR_GrpGetNumObjs( grp );
     for( i = 0; i < size; i++ ) {
         obj = PWR_GrpGetObjByIndx( grp, i );
         for( j = 0; j < count; j++ ) {
-            if( PWR_ObjGetType( obj ) == type )
-                dump_object( obj );
-            dump_type_object( obj, type );
+            if( PWR_ObjGetType( obj ) == type ) {
+                printf( "%s %s", PWR_ObjGetName( obj ),
+                        PWR_ObjGetTypeString( type ) );
+                for( k = 0; k < TOTAL_NUM_PWR_ATTRS; k++ )
+                    printf( " %s", PWR_AttrGetTypeString( k ) );
+                printf( "\n" );
+            }
+            dump_type_objects( obj, type );
         }
     }
-
-    return grp;
 }
 
 int main( int argc, char* argv[] )
