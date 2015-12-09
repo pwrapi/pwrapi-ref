a760650eba4a7098537b1a0d3929285089e63d24
Michael Levenhagen <mjleven@sandia.gov>
2015-12-09 10:52:06 -0700

    add more stuff

--- examples/config/xtpm-node.py
+++ examples/config/xtpm-node.py
@@ -1,190 +1,62 @@
 #!/usr/bin/python
 
-#platName = "plat"
-#numCab  
+from pyConfig import * 
 
-#"plat"
-#"plat.cab0.rack0.nodea"
-
-import re, sys
-
-Platform = "Platform" 
-Cabinet = "Cabinet" 
-Rack = "Rack" 
-Node = "Node" 
-
-Sum = 'Sum'
-Avg = 'Avg'
-
-Energy ='ENERGY'
-Power ='POWER'
-MaxPower ='MAX_POWER'
-
-plugins = {} 
 plugins['CrayXTPM'] = 'libpwr_xtpmdev' 
 
-devices = {}
 devices['XTPM-node'] = ['CrayXTPM','']
 
-def funcName():
-	return 'PY::'+sys._getframe().f_back.f_code.co_name + '(): '
-
-
-class Attr:
-	def __init__(self, op, device, openString):
-		self.op = op
-		self.device = device
-		self.openString = openString
-	def getDevs(self):
-		return [ [self.device, self.openString] ] 
-
-class Object:
-	def __init__(self, type, name, parent = None ):
-		self.type = type 
-		self.name = name
-		self.parent = parent
-		self.numChildren = 0
-		self.child = None
-		self.attrs = {}
-
-	def setChild(self, childObj, numChildren ):
-		self.child = childObj
-		self.numChildren = numChildren 
-
-	def getType(self):
-		return self.type
-
-	def getName(self):
-		return self.name
-
-	def findChildren(self, myName):
-		children = []
-		for i in range(self.numChildren):
-			children = children + \
-				[ myName + '.' + self.child.getName() + str(i) ]	
-
-		return children
-
-	def findAttrChildren(self, myName, attr):
-		print funcName() + 'attr', attr
-		children = []
-
-		if not self.attrs.has_key( attr ):
-			print funcName() + 'attr', attr, 'not found'
-			return None
-
-		for i in range(self.numChildren):
-			children = children + \
-				[ myName + '.' + self.child.getName() + str(i) ]	
-
-		return children
-
-	def setAttrDevice( self, attrName, attr ):
-		print funcName() + 'attr', attrName
-		self.attrs[attrName] = attr
-
-	def getDevs( self, attr ):
-		print funcName() + 'attr', attr
-		if not self.attrs.has_key( attr ):
-			print funcName() + 'attr', attr, 'not found'
-			return None
-
-		return self.attrs[attr].getDevs()
-
-	def traverse( self, func, prefix ='', num = None ):
-		tmp = prefix + self.getName()  
-		if num != None:
-			tmp = tmp + str(num)
-		else:
-			tmp = tmp
-	
-		print tmp 
-		for i in range(self.numChildren):
-			self.child.traverse( func, tmp + '.', i )
-				
 platform = Object( Platform, 'plat' )
-#cabinet = Object( Cabinet, platform )
-#platform.setChild( cabinet, 2 )
-#rack = Object( Rack, cabinet )
-#cabinet.setChild( rack, 1 )
-#node = Object( Node, rack )
-node = Object( Node, 'node', platform )
-platform.setChild( node, 1 )
+platform.setAttrOp( Energy, Sum )
+platform.setAttrOp( Power, Avg )
+platform.setAttrOp( MaxPower, Avg )
+
+cabinet = Object( Cabinet, 'cab', platform )
+cabinet.setAttrOp( Energy, Sum )
+cabinet.setAttrOp( Power, Avg )
+cabinet.setAttrOp( MaxPower, Avg )
 
 
-attr = Attr( Sum, "XTPM-node", "" )
-node.setAttrDevice( Energy, attr )
+board = Object( Board, 'board', cabinet )
+board.setAttrOp( Energy, Sum )
+board.setAttrOp( Power, Avg )
+board.setAttrOp( MaxPower, Avg )
 
-attr = Attr( Avg, "XTPM-node", "f" )
-node.setAttrDevice( Power, attr )
+device = [ "XTPM-node", "" ] 
 
-attr = Attr( Avg, "XTPM-node", "" )
-node.setAttrDevice( MaxPower, attr )
+node = Object( Node, 'node', board )
+node.setAttrOp( Energy, Sum )
+node.setAttrOp( Power, Avg )
+node.setAttrOp( MaxPower, Avg )
+node.addAttrDevice( Energy, device ) 
+node.addAttrDevice( Power, device )
+node.addAttrDevice( MaxPower, device )
 
-objectMap = {}
+platform.setChild( cabinet, 1 )
+cabinet.setChild( board, 1 )
+board.setChild( node, 2 )
+
 objectMap['plat'] = platform
-#objectMap[Cabinet] = cabinet
-#objectMap[Rack] = rack
+objectMap['cab'] = cabinet
+objectMap['board'] = board
 objectMap['node'] = node
 
-def checkName( tmp ):	
-	for index in range(len(tmp)):
+locationMap['plat.cab0.board0'] = 'plat.cab0.board0-router'
+#locationMap['plat.cab0.board1'] = 'plat.cab0.board0-router'
 
-		val = tmp[index]
-		objType = ''.join(ch for ch in val if ch.isalpha() )
-		number = ''.join(ch for ch in val if ch.isdigit() )
-		if not objectMap.has_key( objType ):
-			print "invalid object type", objType
-			return False	
+#print findObjLocation( "plat.cab0.board0.node0" )
 
-	return True
+#print findAttrChildren( "plat", "ENERGY" )
+#print findAttrChildren( "plat.cab0", "ENERGY" )
 
-def getObject( name ):
-	print funcName() + 'object', name
-	tmp = name.split('.')
+#tmp = findAttrChildren( "plat.cab0.board0.node1", "ENERGY" )
+#if not tmp:
+#	print findAttrDevs( "plat.cab0.board0.node1", "ENERGY" )
 
-	if not checkName( tmp ):
-		return None 
-
-	objType = ''.join(ch for ch in tmp[-1] if ch.isalpha() )
-	
-	return objectMap[objType]
-
-
-def getObjType( name ):
-	print funcName() + 'object', name
-	return getObject(name).getType( )
-
-def findChildren( name ):
-	print funcName() + 'object', name
-	return getObject(name).findChildren( name )
-
-def findAttrChildren( name, attr ):
-	print funcName() + 'object', name, 'attr', attr 
-	return getObject(name).findAttrChildren( name, attr )
-
-def findObjDevs( name, attr ):
-	print funcName() + 'object', name
-	return getObject(name).getDevs(attr)
-
-def findPlugins():
-	print funcName() 
-	dictList = []
-	for key, value in plugins.iteritems():
-		temp = [key,value]
-		dictList.append(temp)
-	return dictList 
-
-def findSysDevs():
-	print funcName() 
-	dictList = []
-	for key, value  in devices.iteritems():
-		temp = [key] + value
-		dictList.append(temp)
-	return dictList 
-
-
-print findAttrChildren( "plat.node0", "ENERGY" )
+#print findAttrOp( "plat", "ENERGY" )
+#print findAttrOp( "plat.cab0", "ENERGY" )
+#print findAttrOp( "plat.cab0.board0", "ENERGY" )
+#print findAttrOp( "plat.cab0.board0.node0", "ENERGY" )
 
 #print findSysDevs()
 
