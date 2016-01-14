df27852682d4a43cddddd8437c863c17fc71314f
Michael Levenhagen <mjleven@sandia.gov>
2016-01-14 15:09:58 -0700

    add another version of createNidMap

--- examples/config/batchUtil.py
+++ examples/config/batchUtil.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 
+import re
+
 def expandNidList( tmp ):
       
     ret = []
@@ -21,4 +23,28 @@ def createNidMap( nidList ):
             nidMap += expandNidList(tmp)
         else:
             nidMap += [tmp];
+    return nidMap
+
+def createNidMap2( nidlist):
+    obj = re.findall( r'.*?\[.*?\],*', nidlist )
+
+
+    ret=[]
+    for x in obj:
+        a,b = x.split('[')
+        ret = ret + [ [ a , b.split(']')[0].split(',') ] ]
+
+    nidMap = []
+    for x in ret:
+        a,b = x
+        #print a,b
+        for y in b:
+            tmp = y.split('-')
+            if 2 == len(tmp):
+                #print 'iii'  , tmp
+                for i in range( int(tmp[0]), int(tmp[1]) + 1):
+                    nidMap +=[a + str(i)]
+            else :
+                nidMap += [a + y]
+
     return nidMap
