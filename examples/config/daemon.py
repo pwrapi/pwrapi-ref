e25299f8b568327ba7572c81a3d2bef3f87c5a4f
Michael Levenhagen <mjleven@sandia.gov>
2016-01-15 15:32:41 -0700

    fixup

--- examples/config/daemon.py
+++ examples/config/daemon.py
@@ -1,27 +1,23 @@
 #! /usr/bin/python
 
-nodesPerBoard = 4
-boardsPerCab = 4
 def calcTree( myRank, nidMap ):
-	print "calcTree", len(nidMap)
+	#print "calcTree", len(nidMap)
 
 	if 1 == len(nidMap):
 		return ""
 
 	if 0 == myRank:
 		return ' --rtr.routerInfo={0}:{1}:{2}:{3}:{4}:{5}'.\
-					format( 1, 16000, nidMap[4],16000, 1, 1)
+					format( 1, 16000, nidMap[1],16000, 1, 1)
 	else:
 		return ' --rtr.routerInfo={0}:{1}:{2}:{3}'.\
 					format( 0, 16000, nidMap[0], 16000 )
-		
 
-
-def configRtr( myRank, nidMap, config, routeFile ):
+def configRtr( myRank, nidMap, config, routeFile, nodesPerBoard ):
 	tmp = ""
 
 	if not myRank % nodesPerBoard:
-		if 0 == myRank%nodesPerBoard:
+		if 0 == myRank % nodesPerBoard:
 			tmp += ' --rtr.clientPort=15000'
 		tmp += ' --rtr.serverPort=15001'
 		tmp += ' --rtr.routerType=tree'
@@ -31,7 +27,7 @@ def configRtr( myRank, nidMap, config, routeFile ):
 		tmp += ' --rtr.pwrApiConfig=' + config
 	return tmp
 
-def configBoard( myRank, nidMap, config  ):
+def configBoard( myRank, nidMap, config, nodesPerBoard  ):
 	tmp = ""
 	if not myRank % nodesPerBoard:
 		tmp += ' --srvr0.name=srv0'	
@@ -43,7 +39,7 @@ def configBoard( myRank, nidMap, config  ):
 		tmp += ' --srvr0.pwrApiServerPort=15000'
 	return tmp
 
-def configNode( myRank, nidMap, config ):
+def configNode( myRank, nidMap, config, nodesPerBoard, boardsPerCab ):
 	tmp = ""
 	tmp += ' --srvr.name=srvr'	
 	tmp += ' --srvr.rtrHost=' + nidMap[ myRank/nodesPerBoard * nodesPerBoard ]
@@ -55,14 +51,14 @@ def configNode( myRank, nidMap, config ):
 	tmp += ' --srvr.pwrApiRoot=plat.cab{0}.board{1}.node{2}'.format(cab,board,node)
 	return  tmp
 
-def initDaemon( myRank, nidMap, config, routeFile ):
+def initDaemon( myRank, nidMap, config, routeFile, nodesPerBoard, boardsPerCab ):
 	#print "nidmap={0} myRank={1}".format( nidMap, myRank)
 
 	tmp = '/home/mjleven/pwrGIT/working/build/install/bin/pwrdaemon'
 
-	tmp += configRtr( myRank, nidMap, config, routeFile )
-	tmp += configNode( myRank, nidMap, config )
-	tmp += configBoard( myRank, nidMap, config )
+	tmp += configRtr( myRank, nidMap, config, routeFile,nodesPerBoard )
+	tmp += configNode( myRank, nidMap, config, nodesPerBoard, boardsPerCab )
+	tmp += configBoard( myRank, nidMap, config, nodesPerBoard )
 
 	return tmp	
 
