aa2fddca259a724c3c10eecc974afc0c4a85103d
Michael Levenhagen <mjleven@sandia.gov>
2016-01-15 15:30:31 -0700

    move to another file

--- examples/config/batchUtil.py
+++ examples/config/batchUtil.py
@@ -24,27 +24,3 @@ def createNidMap( nidList ):
         else:
             nidMap += [tmp];
     return nidMap
-
-def createNidMap2( nidlist):
-    obj = re.findall( r'.*?\[.*?\],*', nidlist )
-
-
-    ret=[]
-    for x in obj:
-        a,b = x.split('[')
-        ret = ret + [ [ a , b.split(']')[0].split(',') ] ]
-
-    nidMap = []
-    for x in ret:
-        a,b = x
-        #print a,b
-        for y in b:
-            tmp = y.split('-')
-            if 2 == len(tmp):
-                #print 'iii'  , tmp
-                for i in range( int(tmp[0]), int(tmp[1]) + 1):
-                    nidMap +=[a + str(i)]
-            else :
-                nidMap += [a + y]
-
-    return nidMap
