8e6c25f1e82a763fb3440c6cb7247e6cccdc0746
Michael Levenhagen <mjleven@sandia.gov>
2016-01-28 09:47:11 -0700

    add new logger thread to pwrdaemon

---
+++ tools/pwrdaemon/logger/logger.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014-2016 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this work
+ * by or on behalf of the U.S. Government. Export of this program may require
+ * a license from the United States Government.
+ *
+ * This file is part of the Power API Prototype software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+*/
+
+#ifndef _LOGGER_H
+#define _LOGGER_H
+
+#include <string>
+#include <pwr.h>
+
+
+namespace PWR_Logger {
+
+struct Args {
+    std::string port;
+    std::string host;
+
+    std::string pwrApiConfig;
+    std::string pwrApiRoot;
+    std::string pwrApiServer;
+    std::string pwrApiServerPort;
+    std::string objectName;
+    std::string name;
+    std::string logfile;
+    std::string count;
+    std::string delay;
+};
+
+class Logger {
+  public:
+	Logger( int, char* [] );
+	~Logger();
+	int work();
+  private:
+	Args	m_args;
+	PWR_Cntxt m_ctx;
+	PWR_Obj   m_obj;
+	FILE*	m_logFP;
+	int     m_delay;
+};
+
+}
+
+#endif
