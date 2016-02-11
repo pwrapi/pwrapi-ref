d9342edad9475262dcf8d4461f5c628f4488ba06
David DeBonis <ddeboni@sandia.gov>
2016-02-11 11:33:01 -0700

    cleaning up section messages

--- test/compliance.c
+++ test/compliance.c
@@ -20,42 +20,42 @@
 
 int main( int argc, char* argv[] )
 {
-    int rc = PWR_RET_SUCCESS;
+    int rc, test = PWR_RET_SUCCESS;
 
     printf( "Instantiating compliance test for Section 4.1\n");
-    rc |= section_4_1_test( );
+    test |= (rc=section_4_1_test( ));
     printf( "Results from compliance test for Section 4.1: %s\n", 
             (rc == PWR_RET_SUCCESS ) ? "SUCCESS" : "FAILURE" );
 
     printf( "Instantiating compliance test for Section 4.2\n");
-    rc |= section_4_2_test( );
+    test |= (rc=section_4_2_test( ));
     printf( "Results from compliance test for Section 4.2: %s\n", 
             (rc == PWR_RET_SUCCESS ) ? "SUCCESS" : "FAILURE" );
 
     printf( "Instantiating compliance test for Section 4.3\n");
-    rc |= section_4_3_test( );
+    test |= (rc=section_4_3_test( ));
     printf( "Results from compliance test for Section 4.3: %s\n", 
             (rc == PWR_RET_SUCCESS ) ? "SUCCESS" : "FAILURE" );
 
     printf( "Instantiating compliance test for Section 4.4\n");
-    rc |= section_4_4_test( );
+    test |= (rc=section_4_4_test( ));
     printf( "Results from compliance test for Section 4.4: %s\n", 
             (rc == PWR_RET_SUCCESS ) ? "SUCCESS" : "FAILURE" );
 
     printf( "Instantiating compliance test for Section 4.5\n");
-    rc |= section_4_5_test( );
+    test |= (rc=section_4_5_test( ));
     printf( "Results from compliance test for Section 4.5: %s\n", 
             (rc == PWR_RET_SUCCESS ) ? "SUCCESS" : "FAILURE" );
 
     printf( "Instantiating compliance test for Section 4.6\n");
-    rc |= section_4_6_test( );
+    test |= (rc=section_4_6_test( ));
     printf( "Results from compliance test for Section 4.6: %s\n", 
             (rc == PWR_RET_SUCCESS ) ? "SUCCESS" : "FAILURE" );
 
     printf( "Instantiating compliance test for Section 4.7\n");
-    rc |= section_4_7_test( );
+    test |= (rc=section_4_7_test( ));
     printf( "Results from compliance test for Section 4.7: %s\n", 
             (rc == PWR_RET_SUCCESS ) ? "SUCCESS" : "FAILURE" );
 
-    return rc;
+    return test;
 }
