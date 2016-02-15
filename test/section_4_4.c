48d5f7cf85d82c8914bbfeffa578ec1439e95d5e
David DeBonis <ddeboni@sandia.gov>
2016-02-15 07:46:00 -0700

    found issue with status destroy and fixed

--- test/section_4_4.c
+++ test/section_4_4.c
@@ -33,54 +33,63 @@ int section_4_4_test( void )
     PWR_Status stat;
     PWR_AttrAccessError err; 
 
+    printf( "\tPWR_CntxtInit - application context\n" );
 	rc = PWR_CntxtInit( PWR_CNTXT_DEFAULT, PWR_ROLE_APP, "Application", &cntxt );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: initialization of PowerAPI context failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_CntxtGetEntryPoint\n" );
 	rc = PWR_CntxtGetEntryPoint( cntxt, &self );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: getting self from PowerAPI context failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_ObjAttrGetValue - PWR_ATTR_POWER of self\n" );
     rc = PWR_ObjAttrGetValue( self, PWR_ATTR_POWER, &val, &ts );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: object attribute retrieval failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_ObjAttrSetValue - PWR_ATTR_POWER of self\n" );
     rc = PWR_ObjAttrSetValue( self, PWR_ATTR_POWER, &val );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: object attribute control failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_ObjAttrGetValues - PWR_ATTR_POWER, PWR_ATTR_ENERGY of self\n" );
     rc = PWR_ObjAttrGetValues( self, NUM_ATTR(attrs), attrs, vals, tss, &stat );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: object attributes retrieval failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_ObjAttrSetValues - PWR_ATTR_POWER, PWR_ATTR_ENERGY of self\n" );
     rc = PWR_ObjAttrSetValues( self, NUM_ATTR(attrs), attrs, vals, &stat );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: object attributes control failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_ObjAttrIsValid - PWR_ATTR_POWER of self\n" );
     rc = PWR_ObjAttrIsValid( self, PWR_ATTR_POWER );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: object attribute valid check failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_GrpCreate\n" );
     rc = PWR_GrpCreate( cntxt, &grp );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: creating a group failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_GrpAddObj - self object\n" );
     rc = PWR_GrpAddObj( grp, self );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: adding a object to a group failed\n" );
@@ -88,24 +97,28 @@ int section_4_4_test( void )
     }
 
 #if 0
+    printf( "\tPWR_GrpAttrGetValue - PWR_ATTR_POWER of self group\n" );
     rc = PWR_GrpAttrGetValue( grp, PWR_ATTR_POWER, &val, &ts, &stat );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: group attribute retrieval failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_GrpAttrSetValue - PWR_ATTR_POWER of self group\n" );
     rc = PWR_GrpAttrSetValue( grp, PWR_ATTR_POWER, &val, &stat );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: group attribute control failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_GrpAttrGetValues - PWR_ATTR_POWER, PWR_ATTR_ENERGY of self group\n" );
     rc = PWR_GrpAttrGetValues( grp, NUM_ATTR(attrs), attrs, vals, tss, &stat );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: group attributes retrieval failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_GrpAttrSetValues - PWR_ATTR_POWER, PWR_ATTR_ENERGY of self group\n" );
     rc = PWR_GrpAttrSetValues( grp, NUM_ATTR(attrs), attrs, vals, &stat );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: group attributes control failed\n" );
@@ -113,42 +126,51 @@ int section_4_4_test( void )
     }
 #endif
 
+    printf( "\tPWR_GrpRemoveObj - self object\n" );
     rc = PWR_GrpRemoveObj( grp, self );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: removing a object to a group failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_GrpDestroy - self group\n" );
     rc = PWR_GrpDestroy( grp );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: destroying a group failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_StatusCreate\n" );
 	rc = PWR_StatusCreate( &stat );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: creating status failed\n" );
         return -1;
     }
 
+#if 0
+    printf( "\tPWR_StatusPopError\n" );
     rc = PWR_StatusPopError( stat, &err );  
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: popping error failed\n" );
         return -1;
     }
+#endif
 
+    printf( "\tPWR_StatusClear\n" );
     rc = PWR_StatusClear( stat );  
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: clearing error failed\n" );
         return -1;
     }
 
-    rc = PWR_StatusDestroy( cntxt );
+    printf( "\tPWR_StatusDestroy\n" );
+    rc = PWR_StatusDestroy( stat );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: destruction of status failed\n" );
         return -1;
     }
 
+    printf( "\tPWR_CntxtDestroy\n" );
     rc = PWR_CntxtDestroy( cntxt );
     if( rc != PWR_RET_SUCCESS ) {
         printf( "Error: destruction of PowerAPI context failed\n" );
