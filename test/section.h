f8faeb0105764091c17812d6f90f53a5f37c7d1b
David DeBonis <ddeboni@sandia.gov>
2016-02-23 14:28:10 -0700

    slight modification to make sure assignment operation happens first

--- test/section.h
+++ test/section.h
@@ -13,8 +13,8 @@
 #define SECTION_H
 
 #define RESULT( X ) \
-    (X == PWR_RET_SUCCESS ) ? "SUCCESS" : \
-    ((X == PWR_RET_NOT_IMPLEMENTED) ? "NOT IMPLENTED" : "FAILURE")
+    ((X) == PWR_RET_SUCCESS ) ? "SUCCESS" : \
+    (((X) == PWR_RET_NOT_IMPLEMENTED) ? "NOT IMPLENTED" : "FAILURE")
 
 int section_4_1_test( void );
 int section_4_2_test( void );
