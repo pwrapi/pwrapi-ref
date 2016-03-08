efb823f59ecfdb290f855e03f27670263c23adbe
David DeBonis <ddeboni@sandia.gov>
2016-03-08 09:42:37 -0700

    fix for time in pwrapi tool

--- tools/pwrapi.c
+++ tools/pwrapi.c
@@ -58,7 +58,7 @@ int main( int argc, char* argv[] )
     PWR_Obj self;
     PWR_Cntxt cntxt;
     PWR_Grp grp;
-    time_t time;
+    unsigned long time;
     double start = 0.0, val = 0.0;
     PWR_Time start_ts = 0, val_ts = 0;
     struct timeval t0, t1;
