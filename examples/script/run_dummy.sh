5d2ac88cee2e85f9af679bdb4d1041597a1bca09
Mike <mjleven@sandia.gov>
2016-04-26 14:26:12 -0600

    make work for local xml configuration, move python stuff to another file

--- examples/script/run_dummy.sh
+++ examples/script/run_dummy.sh
@@ -11,7 +11,7 @@
 # distribution.
 #
 
-ROOT=${HOME}/pwrGIT/working
+ROOT=${HOME}/pwrGIT/pwrapi-ref
 
 
 export LD_LIBRARY_PATH="${ROOT}/build/install/lib:${LD_LIBRARY_PATH}"
@@ -19,18 +19,8 @@ export DYLD_LIBRARY_PATH="${ROOT}/build/install/lib:${DYLD_LIBRARY_PATH}"
 
 export POWERAPI_DEBUG=0
 export POWERAPI_ROOT="plat.cab0.board0"
-#export POWERAPI_CONFIG="${ROOT}/examples/config/dummySystem.xml"
+export POWERAPI_CONFIG="${ROOT}/examples/config/dummySystemLocal.xml"
 
 export POWERRT_MACHINE=dummy 
-export POWERAPI_CONFIG=${ROOT}/examples/config/$POWERRT_MACHINE.py
-
-export PYTHONPATH=${ROOT}/examples/config/
-export POWERRT_NUMNODES=4
-export POWERRT_NODES_PER_BOARD=5
-export POWERRT_BOARDS_PER_CAB=2
-
-#export POWERAPI_ROOT="plat"
-#export POWERAPI_SERVER="localhost"
-#export POWERAPI_SERVER_PORT="5000"
 
 $ROOT/build/examples/dummyTest
